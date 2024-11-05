3c175b7dd51b0f24995fe9f8bea874fa7f9eec71
Wiliam-G <wiliam.gomes99@gmail.com>
2024-11-04 23:57:32 -0300

    Implementar as funcionalidades de bouncing da bola nas paredes e nas raquetes, também implementa a contagem de placar

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,17 +12,38 @@ screen.tracer(0)
 
 right_paddle = Paddle(350, 0)
 left_paddle = Paddle(-350,0)
+ball = Ball()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(right_paddle.move_up, "w")
+screen.onkey(right_paddle.move_down, "s")
+
+screen.onkey(left_paddle.move_up, "Up")
+screen.onkey(left_paddle.move_down, "Down")
 
 game_is_on = True
-screen.listen()
 while game_is_on:
-    screen.onkey(right_paddle.move_up, "w")
-    screen.onkey(right_paddle.move_down, "s")
-
-    screen.onkey(left_paddle.move_up, "Up")
-    screen.onkey(left_paddle.move_down, "Down")
-
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
 
+    # detect collision with top and bottom walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #detect collision with right paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # right paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+
+    #left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
 
 screen.exitonclick()
